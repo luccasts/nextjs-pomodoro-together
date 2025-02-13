1afcd42553b285056c46002836242925701e38fa
Lucasts <lucastsilvadev@gmail.com>
2025-02-12 22:42:10 -0300

    fixing profile page and updated firebase

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,11 +1,8 @@
 "use client";
 import { useAuth } from "@/context/AuthContext";
 import styles from "./page.module.scss";
-import { useState } from "react";
 
 export default function Profile() {
-  const [alertMessage, setAlertMessage] = useState<string | null>(null);
-  const [showAlert, setShowAlert] = useState(false);
   const { user } = useAuth();
 
   if (!user) {
@@ -17,24 +14,6 @@ export default function Profile() {
       </div>
     );
   }
-
-  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const file = event.target.files?.[0];
-    if (!file) return;
-
-    const maxSize = 2 * 1024 * 1024; // 2MB
-    if (file.size > maxSize) {
-      setAlertMessage("O arquivo é muito grande! Escolha um menor que 2MB.");
-      setShowAlert(true);
-
-      setTimeout(() => setShowAlert(false), 2500); // Inicia fade-out
-      setTimeout(() => setAlertMessage(null), 3000); // Remove da tela
-      return;
-    }
-
-    console.log("Arquivo válido:", file);
-    // Aqui você pode fazer o upload ou exibir a pré-visualização da imagem
-  };
 
   return (
     <main className={styles.main}>
@@ -55,21 +34,6 @@ export default function Profile() {
             <span>Editar</span>
           </p>
         </div>
-
-        <input
-          type="file"
-          accept="image/gif, image/png, image/jpeg"
-          className={styles.fileInput}
-          onChange={handleFileChange}
-        />
-
-        {alertMessage && (
-          <div
-            className={`${styles.alertBox} ${!showAlert ? styles.hide : ""}`}
-          >
-            {alertMessage}
-          </div>
-        )}
       </section>
     </main>
   );
